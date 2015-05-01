84c2757d1e662da5b7429f139ed22f7f123256b3
imjustinv <imjustinv@gmail.com>
2015-05-01 11:01:32 -0400

    vertexnumber

--- Project5/src/User.java
+++ Project5/src/User.java
@@ -4,14 +4,16 @@ public class User {
 	private LinkedList<User> friends;
 	private String school;
 	private String name;
+	private int vnumber;
 	
 	public User(String name) {
 		this.name = name;
 	}
 	
-	public User(String name, String school) {
+	public User(String name, String school, int vnumber) {
 		this.name = name;
 		this.school = school;
+		this.vnumber = vnumber;
 	}
 	
 	public void addFriend(User u) {
