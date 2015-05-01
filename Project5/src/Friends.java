c820e9cc064ef5c72970814320a57578dc6de868
imjustinv <imjustinv@gmail.com>
2015-04-30 20:48:51 -0400

    updated graphbuild method

--- Project5/src/Friends.java
+++ Project5/src/Friends.java
@@ -1,6 +1,8 @@
 //Justin Valeroso
 //Parth Mehrotra
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,15 +10,18 @@ public class Friends {
 
 	/**
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Please input file name.");
 		String input = sc.nextLine();
 		ArrayList<Node> Graph = GraphBuild(input);
-		
+		for (Node n : Graph){
+			System.out.println(n.name + "|"+ n.school);
+		}
 		System.out.println("Select option: 1. Shortest Path 2. Cliques 3. Connectors 4. Quit ");
 		input = sc.nextLine();
 		
@@ -40,19 +45,36 @@ public class Friends {
 		}
 	}
 	
-	static ArrayList<Node> GraphBuild(String fileName){
+	static ArrayList<Node> GraphBuild(String fileName) throws FileNotFoundException{
 		ArrayList<Node> Graph = new ArrayList<Node>();
-			Scanner sc1 = new Scanner(fileName);
-			String line = sc1.nextLine();
-			while (sc1.hasNext()){
-				line =  sc1.nextLine();
-				for (int i = 0; i < line.length()-1; i++){
-					if(line.charAt(i) == '|'){
-						System.out.println("At " + i);
-						break;
+		
+		Scanner sc1 = new Scanner(new File(fileName));
+		String line = sc1.nextLine();
+		int numOfStudents = Integer.parseInt(line);
+		
+		
+		for(int j = 0; j < numOfStudents; j++){
+			line =  sc1.nextLine();
+			for (int i = 0; i < line.length(); i++){
+				if(line.charAt(i) == '|'){
+					//if student
+					if (line.charAt(i+1) == 'y'){
+						String name = line.substring(0,i);
+						String school = line.substring(i+3,line.length());
+						ArrayList<Node> temp2 = new ArrayList<Node>();
+						Node temp = new Node (name, school, temp2, j);
+						Graph.add(temp);
+					}
+					//if not a student
+					else if (line.charAt(i+1) == 'n'){
+						String name = line.substring(0,i);
+						ArrayList<Node> temp2 = new ArrayList<Node>();
+						Node temp = new Node (name, null, temp2, j);
+						Graph.add(temp);
 					}
 				}
 			}
+		}
 		return Graph;
 	}
 
